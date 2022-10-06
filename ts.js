bc86209a457954d432d5f37d39389e5a24c6cbc6
Nuel Agafie <70712577+Nozel-silva@users.noreply.github.com>
2022-10-06 07:32:36 +0100

    Add files via upload

---
+++ ts.js
@@ -0,0 +1,690 @@
+var names = new Array("Nuel","Faith","Joshua","Favour","collins","Blossom","Sophia","Rachel","Rachael","Uriel", "Rhodes","Rhoda","Patience","Judith","Daniel","Paul","Peter","John","Sophia","Prudence","Larry","Emmanuel", "Shedrach","Pascal","Donaldson","Timothy","Love");
+
+
+
+var input=prompt("Enter your name in the form   name/DD/MM/Y");
+  document.getElementById('demo').innerHTML=input
+          if (input== "Daniel"){
+            let convert = document.querySelector(".convert");
+let speech = new SpeechSynthesisUtterance();
+let textarea = document.querySelector(".paptdadon");
+let volume = document.querySelector(".volume");
+let pitch = document.querySelector(".pitch");
+let rate = document.querySelector(".rate");
+
+  function convertText(){
+
+speech.text = textarea.value;
+speech.pitch = 1;
+speech.volume = 1;
+speech.lang = "en-US";
+speech.rate = 1;
+
+speechSynthesis.speak(speech);
+}
+
+convert.addEventListener("click", ()=>{
+  convertText();
+});
+}
+
+
+ else if (input== "Favour"){
+            let convert = document.querySelector(".convert");
+let speech = new SpeechSynthesisUtterance();
+let textarea = document.querySelector(".favo");
+let volume = document.querySelector(".volume");
+let pitch = document.querySelector(".pitch");
+let rate = document.querySelector(".rate");
+
+  function convertText(){
+
+speech.text = textarea.value;
+speech.pitch = 1;
+speech.volume = 1;
+speech.lang = "en-US";
+speech.rate = 1;
+
+speechSynthesis.speak(speech);
+}
+
+convert.addEventListener("click", ()=>{
+  convertText();
+});
+}
+
+
+ else if (input== "Blessing"){
+            let convert = document.querySelector(".convert");
+let speech = new SpeechSynthesisUtterance();
+let textarea = document.querySelector(".pruang");
+let volume = document.querySelector(".volume");
+let pitch = document.querySelector(".pitch");
+let rate = document.querySelector(".rate");
+
+  function convertText(){
+
+speech.text = textarea.value;
+speech.pitch = 1;
+speech.volume = 1;
+speech.lang = "en-US";
+speech.rate = 1;
+
+speechSynthesis.speak(speech);
+}
+
+convert.addEventListener("click", ()=>{
+  convertText();
+});
+}
+
+else if (input== "Patience"){
+            let convert = document.querySelector(".convert");
+let speech = new SpeechSynthesisUtterance();
+let textarea = document.querySelector(".patbl");
+let volume = document.querySelector(".volume");
+let pitch = document.querySelector(".pitch");
+let rate = document.querySelector(".rate");
+
+  function convertText(){
+
+speech.text = textarea.value;
+speech.pitch = 1;
+speech.volume = 1;
+speech.lang = "en-US";
+speech.rate = 1;
+
+speechSynthesis.speak(speech);
+}
+
+convert.addEventListener("click", ()=>{
+  convertText();
+});
+}
+
+else if (input== "Rhoda"){
+            let convert = document.querySelector(".convert");
+let speech = new SpeechSynthesisUtterance();
+let textarea = document.querySelector(".patbl");
+let volume = document.querySelector(".volume");
+let pitch = document.querySelector(".pitch");
+let rate = document.querySelector(".rate");
+
+  function convertText(){
+
+speech.text = textarea.value;
+speech.pitch = 1;
+speech.volume = 1;
+speech.lang = "en-US";
+speech.rate = 1;
+
+speechSynthesis.speak(speech);
+}
+
+convert.addEventListener("click", ()=>{
+  convertText();
+});
+}
+
+
+else if (input== "Paul"){
+            let convert = document.querySelector(".convert");
+let speech = new SpeechSynthesisUtterance();
+let textarea = document.querySelector(".paptdadon");
+let volume = document.querySelector(".volume");
+let pitch = document.querySelector(".pitch");
+let rate = document.querySelector(".rate");
+
+  function convertText(){
+
+speech.text = textarea.value;
+speech.pitch = 1;
+speech.volume = 1;
+speech.lang = "en-US";
+speech.rate = 1;
+
+speechSynthesis.speak(speech);
+}
+
+convert.addEventListener("click", ()=>{
+  convertText();
+});
+}
+
+else if (input== "Shedrach"){
+            let convert = document.querySelector(".convert");
+let speech = new SpeechSynthesisUtterance();
+let textarea = document.querySelector(".spjsc");
+let volume = document.querySelector(".volume");
+let pitch = document.querySelector(".pitch");
+let rate = document.querySelector(".rate");
+
+  function convertText(){
+
+speech.text = textarea.value;
+speech.pitch = 1;
+speech.volume = 1;
+speech.lang = "en-US";
+speech.rate = 1;
+
+speechSynthesis.speak(speech);
+}
+
+convert.addEventListener("click", ()=>{
+  convertText();
+});
+}
+
+
+else if (input== "Joshua"){
+            let convert = document.querySelector(".convert");
+let speech = new SpeechSynthesisUtterance();
+let textarea = document.querySelector(".spjsc");
+let volume = document.querySelector(".volume");
+let pitch = document.querySelector(".pitch");
+let rate = document.querySelector(".rate");
+
+  function convertText(){
+
+speech.text = textarea.value;
+speech.pitch = 1;
+speech.volume = 1;
+speech.lang = "en-US";
+speech.rate = 1;
+
+speechSynthesis.speak(speech);
+}
+
+convert.addEventListener("click", ()=>{
+  convertText();
+});
+}
+
+else if (input== "Collins"){
+            let convert = document.querySelector(".convert");
+let speech = new SpeechSynthesisUtterance();
+let textarea = document.querySelector(".spjsc");
+let volume = document.querySelector(".volume");
+let pitch = document.querySelector(".pitch");
+let rate = document.querySelector(".rate");
+
+  function convertText(){
+
+speech.text = textarea.value;
+speech.pitch = 1;
+speech.volume = 1;
+speech.lang = "en-US";
+speech.rate = 1;
+
+speechSynthesis.speak(speech);
+}
+
+convert.addEventListener("click", ()=>{
+  convertText();
+});
+}
+
+else if (input== "Solomon"){
+            let convert = document.querySelector(".convert");
+let speech = new SpeechSynthesisUtterance();
+let textarea = document.querySelector(".spjsc");
+let volume = document.querySelector(".volume");
+let pitch = document.querySelector(".pitch");
+let rate = document.querySelector(".rate");
+
+  function convertText(){
+
+speech.text = textarea.value;
+speech.pitch = 1;
+speech.volume = 1;
+speech.lang = "en-US";
+speech.rate = 1;
+
+speechSynthesis.speak(speech);
+}
+
+convert.addEventListener("click", ()=>{
+  convertText();
+});
+}
+
+
+else if (input== "Larry"){
+            let convert = document.querySelector(".convert");
+let speech = new SpeechSynthesisUtterance();
+let textarea = document.querySelector(".lyjnvt");
+let volume = document.querySelector(".volume");
+let pitch = document.querySelector(".pitch");
+let rate = document.querySelector(".rate");
+
+  function convertText(){
+
+speech.text = textarea.value;
+speech.pitch = 1;
+speech.volume = 1;
+speech.lang = "en-US";
+speech.rate = 1;
+
+speechSynthesis.speak(speech);
+}
+
+convert.addEventListener("click", ()=>{
+  convertText();
+});
+}
+
+else if (input== "John"){
+            let convert = document.querySelector(".convert");
+let speech = new SpeechSynthesisUtterance();
+let textarea = document.querySelector(".lyjnvt");
+let volume = document.querySelector(".volume");
+let pitch = document.querySelector(".pitch");
+let rate = document.querySelector(".rate");
+
+  function convertText(){
+
+speech.text = textarea.value;
+speech.pitch = 1;
+speech.volume = 1;
+speech.lang = "en-US";
+speech.rate = 1;
+
+speechSynthesis.speak(speech);
+}
+
+convert.addEventListener("click", ()=>{
+  convertText();
+});
+}
+
+else if (input== "Victor"){
+            let convert = document.querySelector(".convert");
+let speech = new SpeechSynthesisUtterance();
+let textarea = document.querySelector(".lyjnvt");
+let volume = document.querySelector(".volume");
+let pitch = document.querySelector(".pitch");
+let rate = document.querySelector(".rate");
+
+  function convertText(){
+
+speech.text = textarea.value;
+speech.pitch = 1;
+speech.volume = 1;
+speech.lang = "en-US";
+speech.rate = 1;
+
+speechSynthesis.speak(speech);
+}
+
+convert.addEventListener("click", ()=>{
+  convertText();
+});
+}
+
+else if (input== "Peter"){
+            let convert = document.querySelector(".convert");
+let speech = new SpeechSynthesisUtterance();
+let textarea = document.querySelector(".paptdadon");
+let volume = document.querySelector(".volume");
+let pitch = document.querySelector(".pitch");
+let rate = document.querySelector(".rate");
+
+  function convertText(){
+
+speech.text = textarea.value;
+speech.pitch = 1;
+speech.volume = 1;
+speech.lang = "en-US";
+speech.rate = 1;
+
+speechSynthesis.speak(speech);
+}
+
+convert.addEventListener("click", ()=>{
+  convertText();
+});
+}
+
+else if (input== "Faith"){
+            let convert = document.querySelector(".convert");
+let speech = new SpeechSynthesisUtterance();
+let textarea = document.querySelector(".fara");
+let volume = document.querySelector(".volume");
+let pitch = document.querySelector(".pitch");
+let rate = document.querySelector(".rate");
+
+  function convertText(){
+
+speech.text = textarea.value;
+speech.pitch = 1;
+speech.volume = 1;
+speech.lang = "en-US";
+speech.rate = 1;
+
+speechSynthesis.speak(speech);
+}
+
+convert.addEventListener("click", ()=>{
+  convertText();
+});
+}
+
+else if (input== "Rachel"){
+            let convert = document.querySelector(".convert");
+let speech = new SpeechSynthesisUtterance();
+let textarea = document.querySelector(".fara");
+let volume = document.querySelector(".volume");
+let pitch = document.querySelector(".pitch");
+let rate = document.querySelector(".rate");
+
+  function convertText(){
+
+speech.text = textarea.value;
+speech.pitch = 1;
+speech.volume = 1;
+speech.lang = "en-US";
+speech.rate = 1;
+
+speechSynthesis.speak(speech);
+}
+
+convert.addEventListener("click", ()=>{
+  convertText();
+});
+}
+
+else if (input== "Blossom"){
+            let convert = document.querySelector(".convert");
+let speech = new SpeechSynthesisUtterance();
+let textarea = document.querySelector(".BloSOlo");
+let volume = document.querySelector(".volume");
+let pitch = document.querySelector(".pitch");
+let rate = document.querySelector(".rate");
+
+  function convertText(){
+
+speech.text = textarea.value;
+speech.pitch = 1;
+speech.volume = 1;
+speech.lang = "en-US";
+speech.rate = 1;
+
+speechSynthesis.speak(speech);
+}
+
+convert.addEventListener("click", ()=>{
+  convertText();
+});
+}
+
+
+else if (input== "Sophia"){
+            let convert = document.querySelector(".convert");
+let speech = new SpeechSynthesisUtterance();
+let textarea = document.querySelector(".BloSOlo");
+let volume = document.querySelector(".volume");
+let pitch = document.querySelector(".pitch");
+let rate = document.querySelector(".rate");
+
+  function convertText(){
+
+speech.text = textarea.value;
+speech.pitch = 1;
+speech.volume = 1;
+speech.lang = "en-US";
+speech.rate = 1;
+
+speechSynthesis.speak(speech);
+}
+
+convert.addEventListener("click", ()=>{
+  convertText();
+});
+}
+
+
+else if (input== "Esther"){
+            let convert = document.querySelector(".convert");
+let speech = new SpeechSynthesisUtterance();
+let textarea = document.querySelector(".BloSOlo");
+let volume = document.querySelector(".volume");
+let pitch = document.querySelector(".pitch");
+let rate = document.querySelector(".rate");
+
+  function convertText(){
+
+speech.text = textarea.value;
+speech.pitch = 1;
+speech.volume = 1;
+speech.lang = "en-US";
+speech.rate = 1;
+
+speechSynthesis.speak(speech);
+}
+
+convert.addEventListener("click", ()=>{
+  convertText();
+});
+}
+
+else if (input== "Love"){
+            let convert = document.querySelector(".convert");
+let speech = new SpeechSynthesisUtterance();
+let textarea = document.querySelector(".BloSOlo");
+let volume = document.querySelector(".volume");
+let pitch = document.querySelector(".pitch");
+let rate = document.querySelector(".rate");
+
+  function convertText(){
+
+speech.text = textarea.value;
+speech.pitch = 1;
+speech.volume = 1;
+speech.lang = "en-US";
+speech.rate = 1;
+
+speechSynthesis.speak(speech);
+}
+
+convert.addEventListener("click", ()=>{
+  convertText();
+});
+}
+
+else if (input== "Stella"){
+            let convert = document.querySelector(".convert");
+let speech = new SpeechSynthesisUtterance();
+let textarea = document.querySelector(".BloSOlo");
+let volume = document.querySelector(".volume");
+let pitch = document.querySelector(".pitch");
+let rate = document.querySelector(".rate");
+
+  function convertText(){
+
+speech.text = textarea.value;
+speech.pitch = 1;
+speech.volume = 1;
+speech.lang = "en-US";
+speech.rate = 1;
+
+speechSynthesis.speak(speech);
+}
+
+convert.addEventListener("click", ()=>{
+  convertText();
+});
+}
+
+
+else if (input== "Daisy"){
+            let convert = document.querySelector(".convert");
+let speech = new SpeechSynthesisUtterance();
+let textarea = document.querySelector(".BloSOlo");
+let volume = document.querySelector(".volume");
+let pitch = document.querySelector(".pitch");
+let rate = document.querySelector(".rate");
+
+  function convertText(){
+
+speech.text = textarea.value;
+speech.pitch = 1;
+speech.volume = 1;
+speech.lang = "en-US";
+speech.rate = 1;
+
+speechSynthesis.speak(speech);
+}
+
+convert.addEventListener("click", ()=>{
+  convertText();
+});
+}
+
+
+else if (input== "Prudence"){
+            let convert = document.querySelector(".convert");
+let speech = new SpeechSynthesisUtterance();
+let textarea = document.querySelector(".pruang");
+let volume = document.querySelector(".volume");
+let pitch = document.querySelector(".pitch");
+let rate = document.querySelector(".rate");
+
+  function convertText(){
+
+speech.text = textarea.value;
+speech.pitch = 1;
+speech.volume = 1;
+speech.lang = "en-US";
+speech.rate = 1;
+
+speechSynthesis.speak(speech);
+}
+
+convert.addEventListener("click", ()=>{
+  convertText();
+});
+}
+
+else if (input== "Angel"){
+            let convert = document.querySelector(".convert");
+let speech = new SpeechSynthesisUtterance();
+let textarea = document.querySelector(".pruang");
+let volume = document.querySelector(".volume");
+let pitch = document.querySelector(".pitch");
+let rate = document.querySelector(".rate");
+
+  function convertText(){
+
+speech.text = textarea.value;
+speech.pitch = 1;
+speech.volume = 1;
+speech.lang = "en-US";
+speech.rate = 1;
+
+speechSynthesis.speak(speech);
+}
+
+convert.addEventListener("click", ()=>{
+  convertText();
+});
+}
+
+else if (input== "Emmanuella"){
+            let convert = document.querySelector(".convert");
+let speech = new SpeechSynthesisUtterance();
+let textarea = document.querySelector(".pruang");
+let volume = document.querySelector(".volume");
+let pitch = document.querySelector(".pitch");
+let rate = document.querySelector(".rate");
+
+  function convertText(){
+
+speech.text = textarea.value;
+speech.pitch = 1;
+speech.volume = 1;
+speech.lang = "en-US";
+speech.rate = 1;
+
+speechSynthesis.speak(speech);
+}
+
+convert.addEventListener("click", ()=>{
+  convertText();
+});
+}
+
+else if (input== "Kennedy"){
+            let convert = document.querySelector(".convert");
+let speech = new SpeechSynthesisUtterance();
+let textarea = document.querySelector(".ken");
+let volume = document.querySelector(".volume");
+let pitch = document.querySelector(".pitch");
+let rate = document.querySelector(".rate");
+
+  function convertText(){
+
+speech.text = textarea.value;
+speech.pitch = 1;
+speech.volume = 1;
+speech.lang = "en-US";
+speech.rate = 1;
+
+speechSynthesis.speak(speech);
+}
+
+convert.addEventListener("click", ()=>{
+  convertText();
+});
+}
+
+else if (input== "Nuel"){
+            let convert = document.querySelector(".convert");
+let speech = new SpeechSynthesisUtterance();
+let textarea = document.querySelector(".nuel");
+let volume = document.querySelector(".volume");
+let pitch = document.querySelector(".pitch");
+let rate = document.querySelector(".rate");
+
+  function convertText(){
+
+speech.text = textarea.value;
+speech.pitch = 1;
+speech.volume = 1;
+speech.lang = "en-US";
+speech.rate = 1;
+
+speechSynthesis.speak(speech);
+}
+
+convert.addEventListener("click", ()=>{
+  convertText();
+});
+}
+
+
+else{
+
+let convert = document.querySelector(".convert");
+let speech = new SpeechSynthesisUtterance();
+let textarea = document.querySelector(".daniel");
+let volume = document.querySelector(".volume");
+let pitch = document.querySelector(".pitch");
+let rate = document.querySelector(".rate");
+
+  function convertText(){
+
+speech.text = textarea.value;
+speech.pitch = 1;
+speech.volume = 1;
+speech.lang = "en-US";
+speech.rate = 1;
+
+speechSynthesis.speak(speech);
+}
+
+convert.addEventListener("click", ()=>{
+  convertText();
+});
+}
+
+  
